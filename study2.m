ea8213212ec4cd835647f749b1d4b5924a2949df
frefr166 <frefr166@student.liu.se>
2015-10-04 13:08:05 +0200

    La till korrekta histogram, la till sista teoretiska PSD

--- study2.m
+++ study2.m
@@ -21,7 +21,7 @@ Yhalf = stepfunction(filter_noise);
 
 %% AM-SC modulator
 
-omega0 = (2*pi*(theta0/2));
+omega0 = (2*pi*(theta0/2) + 0.0);
 
 Yamsc = (filter_noise' .* cos(omega0 * n))';
 
@@ -44,12 +44,59 @@ subplot(133);
 plot(theta_norm, Yamsc_per);
 title('Yamsc nonlin')
 
-%% Create theoretical PSD:s
+%% Histograms
+figure(2);
+subplot(141)
+histogram(Ysquare,100);
+title('PSDsquare nonlin')
+subplot(142)
+histogram(Yhalf,100);
+title('PSDhalf nonlin')
+subplot(143)
+histogram(Yamsc,100);
+title('PSDamsc nonlin')
+subplot(144)
+histogram(filter_noise,100);
+title('blackPSD2 lin')
 
-%Ysquared_theor=
+% Varför blir den sista PSD:en inte så gaussisk som vi vill...?
+% Kanske så att den är 52 bred och 95 hög...?
+% 
+
+%% Nonlinears
+
+figure(3)
+plot(thetablack, blackPSD2)
+%%
+figure(2);
+subplot(131)
+histoPSDsquare
+subplot(132)
+histoPSDhalf
+subplot(133)
+histoPSDamsc
+
+%% Create theoretical PSD:s
+%TODO: SKA VI SPEGLA SÅ VI FÅR EN DUBBELSIDIG?
+% AM SSB SC? AM SC?
+% SKA VI TA BORT DIRAC:EN?
+% VARFÖR BLIR VÅR AMSC FÖRSKJUTEN? SKA VI LÄGGA PÅ EN FASFÖRSKJUTNING? 0.2?
+%HUR MKT VIKT SKA LÄGGAS PÅ AMPLITUDER NÄR VI JÄMFÖR, TYP ESTIMERADE I LAB1
+% AMSC ÄR VÄLDIGT LIK BLACKPSD2 (LINJÄR).
+% OM MAN PLOTTAR DEN TEORETISKA RY2 FÅR VI TVÅ STOLPAR. ÄR DET GAUSSISKT?
+% KAN EN PSD VARA GAUSSISK?
+% VF FÖRSVINNER HALVA PLOTTEN? SAMMA MED VÅRT IDEALA LP.
+    % HAR MATLAB PROBLEM MED RECT?
+
+ysquaredC = R0^2/(theta0^2);
+ysquared1 = dirac(theta_norm);
+ysquared2 = 2/(theta0) * tripuls(theta_norm /theta0);
+ysquared3 = 2/(theta0) * tripuls((theta_norm -1 )/theta0);
+
+Ysquared_theor= ysquaredC * (ysquared1 + ysquared2 + ysquared3);
 
 yhalfC = R0/(2 * theta0^2);
-yhalf1 = dirac(theta_norm)/pi;
+yhalf1 = 1;% dirac(theta_norm)/pi;
 yhalf2 = 1/2 * rectpuls(theta_norm /theta0);
 yhalf3 = theta0/(2 * pi) * tripuls(theta_norm/theta0);
 yhalf4 = 1/2 * rectpuls(-theta_norm /theta0);
@@ -60,16 +107,26 @@ Yhalf_theor = yhalfC * (yhalf1 + yhalf2 + yhalf3 + yhalf4 + yhalf5);
 yamscC = R0/(4 * theta0^2);
 yamsc1 = rectpuls((theta_norm+omega0)/theta0);
 yamsc2 = rectpuls((theta_norm-omega0)/theta0);
+yamsc3 = rectpuls((theta_norm-1+omega0)/theta0);
+yamsc4 = rectpuls((theta_norm-1 -omega0)/theta0);
 
-Yamsc_theor = yamscC * (yamsc1 + yamsc2);
+Yamsc_theor = yamscC * (yamsc1 + yamsc2 + yamsc3 + yamsc4);
 
+%% Plot periodograms
 figure(2);
-%subplot(131);
-%plot(theta_norm,Ysquare_theor);
-%title('Ysquare nonlin theor')
+subplot(131);
+plot(theta_norm,fftshift(abs(Ysquared_theor)));
+title('Ysquare nonlin theor')
 subplot(132);
 plot(theta_norm, fftshift(abs(Yhalf_theor)));
 title('Yhalf nonlin theor')
 subplot(133);
 plot(theta_norm, Yamsc_theor);
 title('Yamsc nonlin theor')
+%%
+figure(3);
+Rzhwt = 1/(4*pi)*(tripuls(theta_norm/(2*theta0))+tripuls((theta_norm-1)/(2*theta0)))+...
+    +1/4*(rectpuls(theta_norm/(2*theta0))+rectpuls((theta_norm-1)/(2*theta0)));
+Rzhwt(1) = Rzhwt(1)+theta0/pi;
+plot(theta_norm, fftshift(abs(Rzhwt)));
+title('Yamsc nonlin theor david')
